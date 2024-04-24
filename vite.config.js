f50d0c9b3ccc3694f867a604cf4f36c11c3b5350
Robyn <Robyn796@mac.com>
2024-04-24 13:16:13 -0600

    wednesday meeting activity

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-
 export default defineConfig({
   root: "src/",
 
@@ -13,16 +12,16 @@ export default defineConfig({
         checkout: resolve(__dirname, "src/checkout/index.html"),
         product1: resolve(
           __dirname,
-          "src/product_pages/cedar-ridge-rimrock-2.html"
+          "src/product_pages/cedar-ridge-rimrock-2.html",
         ),
         product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
         product3: resolve(
           __dirname,
-          "src/product_pages/northface-alpine-3.html"
+          "src/product_pages/northface-alpine-3.html",
         ),
         product4: resolve(
           __dirname,
-          "src/product_pages/northface-talus-4.html"
+          "src/product_pages/northface-talus-4.html",
         ),
       },
     },
