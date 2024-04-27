fd72456f8ca5d8bc507416ec4f2c83f1ea5ad8b2
ndongaloris <ndongajared@gmail.com>
2024-04-27 22:23:04 +0200

    fixing vite 3

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,10 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
+        main: [
+            resolve(__dirname, "src/index.html"),
+            resolve(__dirname, "src/js/main.js")
+          ],
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
         product1: resolve(
