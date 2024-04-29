8fd075d3a45b4f9f8845258e6f47c629eca0a186
ndongaloris <ndongajared@gmail.com>
2024-04-29 18:48:54 +0200

    test two vite file

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/js/main.js"),
+        main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
         product1: resolve(
