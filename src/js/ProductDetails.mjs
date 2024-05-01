4ed888e960a7307bf1fd1c085eaf1d74ffb2986f
ndongaloris <ndongajared@gmail.com>
2024-05-01 21:40:11 +0200

    step 6 6try

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -31,7 +31,7 @@ export default class ProductDetails{
         // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
         this.product = await this.dataSource.findProductById(this.productId);
         // once we have the product details we can render out the HTML
-        this.renderTemplate("main");
+        this.renderProductDetails("main");
         // this.renderProductDetails(this.product);
         // once the HTML is rendered we can add a listener to Add to Cart button
         // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage, getLocalStorage, getParams } from "./utils.mjs";
+import { getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
