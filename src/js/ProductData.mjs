046d60a5738fc4c418926a937f6893fb3c603a39
ndongaloris <ndongajared@gmail.com>
2024-05-01 20:27:24 +0200

    Url paramaters

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -21,3 +21,9 @@ export default class ProductData {
     return products.find((item) => item.Id === id);
   }
 }
+
+export function getParams(param){
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get('product')
+}
--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage, getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
