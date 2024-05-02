cdc8171df3a8016faba25526f9d57faa454ec999
ndongaloris <ndongajared@gmail.com>
2024-05-02 11:21:54 +0200

    .

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -32,7 +32,7 @@ export default class ProductDetails{
         // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
         this.product = await this.dataSource.findProductById(this.productId);
         // once we have the product details we can render out the HTML
-        this.renderProductDetail("main");
+        this.renderProductDetails("main");
         // once the HTML is rendered we can add a listener to Add to Cart button
         // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
         // add listener to Add to Cart button
@@ -44,7 +44,7 @@ export default class ProductDetails{
         this.productList.push(this.product);
         setLocalStorage("so-cart", this.productList);
     }    
-    renderProductDetail(selector){
+    renderProductDetails(selector){
         const element = document.querySelector(selector);
         element.insertAdjacentHTML("afterbegin", renderTemplate(this.product)
         );
