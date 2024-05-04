aa9236b44c8cf953a88d1ea42929e080cef60021
Fortue3 <kwe22002@byui.edu>
2024-05-04 19:16:53 +0200

    remove item

--- src/js/cart.js
+++ src/js/cart.js
@@ -26,3 +26,34 @@ function cartItemTemplate(item) {
 }
 
 renderCartContents();
+function cartItemTemplate(item) {
+  const newItem = `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${item.Image}"
+      alt="${item.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${item.FinalPrice}</p>
+  <div class="cart-card__del-btn">
+    <span id="removeFromCart" data-id="${item.Id}">X</span>
+  </div>
+</li>`;
+
+  return newItem;
+}
+
+renderCartContents();
+
+// Add event listener to all "Remove" buttons dynamically
+document.addEventListener('click', function(event) {
+  if (event.target.id === 'removeFromCart') {
+    const prodId = event.target.dataset.id;
+    removeFromCart(prodId);
+  }
+});
--- src/js/product.js
+++ src/js/product.js
@@ -17,3 +17,13 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document.getElementById("addToCart").addEventListener("click", addToCartHandler);
+
+import { getParams } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+
+const productId = getParams("product");
+const dataSource = new ProductData("tents");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
