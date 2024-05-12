bbea1e372ad3453b777464a537278aef3ab66e04
Tarin James <122317277+tarin-james@users.noreply.github.com>
2024-05-11 20:35:09 -0600

    discount indicator added

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,8 +1,12 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
-function productCardTemplate(product){
-    return `<li class="product-card">
+function productCardTemplate(product) {
+  console.log(product)
+  const discounted = product.FinalPrice < product.SuggestedRetailPrice ;
+  const discount = (product.SuggestedRetailPrice - product.FinalPrice) / product.SuggestedRetailPrice * 100 ;
+  return `<li class="product-card">
                 <a href="../product_pages/index.html?product=${product.Id}">
+                ${discounted && `<div class="discount"> ${Math.round(discount)}% off</div>`}
                 <picture>
                 <source srcset="${product.Images.PrimarySmall}" media="(max-width: 700px)">
                     <img
@@ -13,21 +17,23 @@ function productCardTemplate(product){
                 <h2 class="card__name">${product.Name}</h2>
                 <p class="product-card__price">${product.FinalPrice}</p></a
                 >
-            </li>`
+            </li>`;
 }
 
-export default class productList{
-    constructor(category, dataSource, listElement){
-        this.dataSource = dataSource;
-        this.category = category;
-        this.listElement = listElement;
-    }
-    async init(){
-        
-        const list = await this.dataSource.getData(this.category);
-        this.renderList(list);
-    }
-    renderList(list){
-        renderListWithTemplate(productCardTemplate, this.listElement, list)
-    }
+export default class productList {
+  constructor(category, dataSource, listElement) {
+    this.dataSource = dataSource;
+    this.category = category;
+    this.listElement = listElement;
+  }
+  async init() {
+    const list = await this.dataSource.getData(this.category);
+    console.log(list);
+    this.renderList(list);
+  }
+  renderList(list) {
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+  }
 }
+
+
