e7fb1023fb90f808403d6d01b42bb629c0eb2cf5
ndongaloris <ndongajared@gmail.com>
2024-05-15 15:28:58 +0200

    News letter section added and finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import Alert from "../js/Alert.js"
 import { loadHeaderFooter } from "./utils.mjs"
+import Newsletter from "./Newsletter.mjs";
 
 loadHeaderFooter();
 
 const alert = new Alert();
 alert.displayAlerts();
+
+const newsletter = new Newsletter();
+newsletter.init();
